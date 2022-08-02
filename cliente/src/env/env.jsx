f6601707e06e0faa8ad0c8ab7718b084b88d723e
kelsier94 <abulpe.alumno@iesrafaelalberti.es>
2022-08-02 20:20:42 +0200

    nuevos cambios en env 2.0

--- cliente/src/env/env.jsx
+++ cliente/src/env/env.jsx
@@ -1,5 +1,5 @@
 export const googleMapKey = 'AIzaSyBfkSuyy45Oorhy9Cnb3V9_e-8g0QPUMoY'
 
 // export const serverApis = ["http://localhost:3001"]
-export const serverApis = ["https://https://servidor-production.up.railway.app/"]
+export const serverApis = ["https://servidor-production.up.railway.app/"]
 // export const serverApis = ['/']
