2ad804956bc25895a486ede0729d76e5e29b2da1
segundaaguada <108788046+segundaaguada@users.noreply.github.com>
2022-08-25 12:25:43 +0200

    Update env.jsx

--- cliente/src/env/env.jsx
+++ cliente/src/env/env.jsx
@@ -1,5 +1,5 @@
 export const googleMapKey = 'AIzaSyBfkSuyy45Oorhy9Cnb3V9_e-8g0QPUMoY'
 
 // export const serverApis = ["http://localhost:3001"]
-export const serverApis = ["http://68.183.140.8:3001/"]
+export const serverApis = ["http://68.183.140.8/"]
 // export const serverApis = ['/']
